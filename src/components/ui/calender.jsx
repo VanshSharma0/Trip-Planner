4223f92839214a50e43216072c8f6793a75d9ef6
VanshSharma0 <vanshsharma9933@gmail.com>
2025-02-28 14:26:40 +0530

    Refactor calendar component to import useState directly from React

--- src/components/ui/calender.jsx
+++ src/components/ui/calender.jsx
@@ -1,5 +1,5 @@
 // src/components/ui/calendar/calendar.jsx
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { DayPicker } from 'react-day-picker';
 import 'react-day-picker/dist/style.css'; 
 
