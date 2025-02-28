70ad147c3cc2a19bc8a4a3fcf27b1dedf5087cba
VanshSharma0 <vanshsharma9933@gmail.com>
2025-02-28 14:14:02 +0530

    Fix import path for Button component and add index file for Calendar component

--- src/components/ui/button/index.jsx
+++ src/components/ui/button/index.jsx
@@ -1,5 +1,5 @@
 // src/components/ui/button/index.js
 
-import Button from './button'; // Assuming you have a Button component in button.jsx
+import Button from '../button'; // Assuming you have a Button component in button.jsx
 
 export default Button; 
---
+++ src/components/ui/calendar/index.jsx
@@ -0,0 +1,5 @@
+// src/components/ui/calendar/index.js
+
+import Calendar from './calendar'; // Assuming you have a Calendar component in calendar.jsx
+
+export default Calendar; 
